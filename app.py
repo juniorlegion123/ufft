1cbb7c565a2f9fbcd520018b9d5602f0b7b15e29
juniorlegion123 <shubhamprabhu02@gmail.com>
2025-04-30 22:08:11 +0530

    Remove sensitive info

--- app.py
+++ app.py
@@ -19,7 +19,10 @@ import plotly.graph_objects as go
 from apscheduler.schedulers.background import BackgroundScheduler 
 import pdfkit  
 from threading import Thread
+import os
+from dotenv import load_dotenv
 # from flask_migrate import Migrate
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "unifiedfamilyfinancetracker"
@@ -38,8 +41,8 @@ db.init_app(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'unifiedfamilyfinancetracker1@gmail.com'
-app.config['MAIL_PASSWORD'] = 'fmmt usra jono qbed'  # Use App Password
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'unifiedfamilyfinancetracker1@gmail.com'  # Add this line
 mail = Mail(app)
 
